25f2e63b9d850387b6b75ef2b07b76fdd1160707
Daniil <55439154+vend1k12@users.noreply.github.com>
2025-05-08 11:36:07 +0300

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -54,3 +54,12 @@ krabby random --no-title # showing random pokemon on startup pwsh
 # INITIALIZATION
 #-----------------------------------------------------
 Invoke-Expression (& { (zoxide init powershell | Out-String) }) # init pwsh with zoxide
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
